1cf07c03a1d69afde65f8e65422505c43b1b96b1
Chima Nwosu <chimannwosu@gmail.com>
2023-09-24 21:35:45 +0300

    Code clean up

--- app.ts
+++ app.ts
@@ -9,11 +9,10 @@ const _tableName = "greendoors";
 const x = new dynamo.DocumentClient({
   region: "eu-west-2",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   }
 });
-
 const app: Application = express();
 app.use(express.json())  
 app.use(cors())  
